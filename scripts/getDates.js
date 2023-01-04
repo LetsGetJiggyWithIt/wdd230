539653a3631bd2c79fbb15746697f16840f8f3a9
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-04 15:23:09 -0700

    Major changes to index.html, as well as replacement of styles.css with base.css.

---
+++ scripts/getDates.js
@@ -0,0 +1,3 @@
+document.getElementById('lastModified').innerHTML += document.lastModified;
+currentYear = new Date().getFullYear();
+document.getElementById('copyright').innerHTML = "Copyright " + currentYear + " Vaughn Palmer, Utah, United States"
