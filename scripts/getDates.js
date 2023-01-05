6f1349698dffc86f31b29842b4929af51ec6acd2
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-05 14:00:19 -0700

    Added chamber-site-plan.html and link to it on homepage. Minor changes to footer and javascript files.

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,3 @@
 document.getElementById('lastModified').innerHTML += document.lastModified;
 currentYear = new Date().getFullYear();
-document.getElementById('copyright').innerHTML = "Copyright " + currentYear + " Vaughn Palmer, Utah, United States"
+document.getElementById('copyright').innerHTML += `${currentYear} Vaughn Palmer, Utah, United States`
