072059ff1e96835f11b5e96b99614f24653af125
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-12 16:52:45 -0700

    Completed all mid-week assignments

--- scripts/menu.js
+++ scripts/menu.js
@@ -2,10 +2,12 @@ const menuButton = document.querySelector('#menu-button');
 const menuItems = document.querySelectorAll('.menu-item');
 const menuIcon = document.querySelector('#menu-icon');
 const menu = document.querySelector('.menu');
+const fillerDiv = document.querySelector('#filler-div');
 
 menuButton.addEventListener("click", () => {
 	menuItems.forEach((item) => item.classList.toggle("open"));
 	menuButton.classList.toggle("close");
     menuIcon.classList.toggle("close");
     menu.classList.toggle("open");
+    fillerDiv.classList.toggle("close");
 });
