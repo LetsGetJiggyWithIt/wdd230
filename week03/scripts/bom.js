28f38aee42f8f2750180c5ff970bddc106c371a4
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-19 14:49:23 -0700

    Completed all preparation assignments

--- week03/scripts/bom.js
+++ week03/scripts/bom.js
@@ -1,22 +1,47 @@
 const input = document.querySelector("#favchap");
 const button = document.querySelector("button");
 const list = document.querySelector("#list");
-const chapters = getChapterList() || [];
+let chapters = getChapterList() || [];
 
-
+chapters.forEach(chapter => {
+    displayList(chapter);
+});
 
 button.addEventListener("click", () => {
     if (input.value != "") {
-        const li = document.createElement("li");
-        const deleteButton = document.createElement("button");
-        li.textContent = input.value;
-        deleteButton.textContent = "❌";
-        li.append(deleteButton);
-        list.append(li);
-        deleteButton.addEventListener("click", () => {
-            list.removeChild(li);
-            input.focus;
-            input.value = "";
-        });
+        displayList(input.value);
+        chapters.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
     }
 });
+
+function displayList (item) {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+    li.textContent = item;
+    deleteButton.textContent = "❌";
+    li.append(deleteButton);
+    list.append(li);
+    deleteButton.addEventListener("click", () => {
+        deleteChapter(li.textContent);
+        list.removeChild(li);
+        input.focus;
+        input.value = "";
+    });
+};
+
+function setChapterList () {
+    window.localStorage.setItem("myFavBomList", JSON.stringify(chapters));
+}
+
+function getChapterList () {
+    return JSON.parse(window.localStorage.getItem("myFavBomList"));
+}
+
+function deleteChapter (chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chapters = chapters.filter((item) => item !== chapter);
+    setChapterList();
+}
