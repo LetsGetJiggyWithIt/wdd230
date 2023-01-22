4030eb52712e87cd53eacd916cc65c2080ff5762
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-21 17:51:17 -0700

    Completed week03 Chamber Site Milestone Project

--- chamber/scripts/darkModeToggle.js
+++ chamber/scripts/darkModeToggle.js
@@ -1,6 +1,7 @@
 const modeButton = document.querySelector("#dark-mode");
 const main = document.querySelector("main");
 const cards = document.querySelectorAll(".card");
+const images = document.querySelectorAll(".image-div");
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("Dark Mode☑️")) {
@@ -8,11 +9,13 @@ modeButton.addEventListener("click", () => {
 		main.style.background = "#1b712a";
 		main.style.color = "var(--tea-green)";
         cards.forEach(darkCard);
+		images.forEach(darkCard);
 		modeButton.textContent = "Dark Mode❎";
 	} else {
 		main.style.background = "var(--dark-sea-green)";
 		main.style.color = "#1b712a";
         cards.forEach(lightCard);
+		images.forEach(lightCard);
         modeButton.classList.toggle("active");
 		modeButton.textContent = "Dark Mode☑️";
 	}
---
+++ chamber/scripts/getVisits.js
@@ -0,0 +1,31 @@
+const visitsLabel1 = document.querySelector('#userVisits1');
+const visitsLabel2 = document.querySelector('#userVisits2');
+let visits = Number(window.localStorage.getItem("visits-ls"));
+const currentDate = new Date();
+const lastMilliseconds = Number(window.localStorage.getItem("visits-time"));
+const currentMilliseconds = Number(currentDate.getTime());
+const daysSinceLastVisit = GetDaysSinceLastVisit(lastMilliseconds, currentMilliseconds);
+function GetDaysSinceLastVisit(last, current) {
+    current = Math.round(current / 60000 / 60 / 24);
+    last = Math.round(last / 60000 / 60 / 24);
+    days = current - last;
+    return days;
+}
+if (visits !== 0) {
+    if (visits == 1) {
+        visitsLabel1.textContent = "You have visited this site " + visits + " time.";
+    }
+    else {
+        visitsLabel1.textContent = "You have visited this site " + visits + " times.";
+    }
+    if (visits >= 50) {
+        visitsLabel1.textContent += "👀";
+    }
+}
+else {
+    visitsLabel1.textContent = "This is your first time!";
+}
+visitsLabel2.textContent = "It has been " + daysSinceLastVisit + " days.";
+visits++;
+localStorage.setItem("visits-ls", visits);
+localStorage.setItem("visits-time", currentMilliseconds);
