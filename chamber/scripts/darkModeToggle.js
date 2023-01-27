461a069f999847202d1c446ceb2994d7a9310ded
LetsGetJiggyWithIt <vpalmscholar@gmail.com>
2023-01-27 14:07:12 -0700

    Added localStorage for user's darkmode preferences

--- chamber/scripts/darkModeToggle.js
+++ chamber/scripts/darkModeToggle.js
@@ -2,8 +2,15 @@ const modeButton = document.querySelector("#dark-mode");
 const main = document.querySelector("main");
 const cards = document.querySelectorAll(".card");
 const images = document.querySelectorAll(".image-div");
+let darkModeOn = window.localStorage.getItem("darkMode-ls");
 
-modeButton.addEventListener("click", () => {
+modeButton.addEventListener("click", toggleMode);
+
+if (darkModeOn == "true") {
+	toggleMode();
+}
+
+function toggleMode () {
 	if (modeButton.textContent.includes("Dark Mode☑️")) {
         modeButton.classList.toggle("active");
 		main.style.background = "#1b712a";
@@ -11,6 +18,7 @@ modeButton.addEventListener("click", () => {
         cards.forEach(darkCard);
 		images.forEach(darkCard);
 		modeButton.textContent = "Dark Mode❎";
+		window.localStorage.setItem("darkMode-ls", "true");
 	} else {
 		main.style.background = "var(--dark-sea-green)";
 		main.style.color = "#1b712a";
@@ -18,8 +26,11 @@ modeButton.addEventListener("click", () => {
 		images.forEach(lightCard);
         modeButton.classList.toggle("active");
 		modeButton.textContent = "Dark Mode☑️";
+		window.localStorage.setItem("darkMode-ls", "false");
 	}
-});
+	console.log(window.localStorage.getItem("darkMode-ls"));
+}
+
 function lightCard (item) {
     item.style.background = "var(--blue-ncs)";
     item.style.color = "#1b712a";
